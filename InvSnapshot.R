e8c2c12b2738c561c68db277e2b0bb190362bdf0
maxwrobl <69998525+maxwrobl@users.noreply.github.com>
2020-12-15 10:34:29 -0500

    Update InvSnapshot.R

--- InvSnapshot.R
+++ InvSnapshot.R
@@ -1,22 +1,22 @@
-library("dplyr")
-library("shiny") 
-library("streamgraph")
-library("htmlwidgets")
+library(dplyr)
+library(shiny) 
+library(streamgraph)
+library(htmlwidgets)
 
 stream<-read.csv("snapshot.csv")
 
-##Cleaning Date Format
+#Cleaning Date Format
 stream$snapshotDate <- as.factor(stream$snapshotDate)
 abis<-strptime(stream$snapshotDate,format = "%m/%d/%Y")
 stream$snapshotDate<-as.Date(abis,format = "%Y-%m-%d")
 
-##Cleaning Data
+#Cleaning Data
 stream$dieLocation<-trimws(stream$dieLocation)
 stream$itemNumber<-trimws(stream$itemNumber)
 stream$quantityOnHand <- gsub(",","",stream$quantityOnHand)
 stream$quantityOnHand <- as.numeric(stream$quantityOnHand)
 
-##Formatting User Interface
+#Formatting User Interface
 ui <- fluidPage(
   h2("Inventory History by Item", style="text-align:left"),
   br(),
@@ -24,26 +24,27 @@ ui <- fluidPage(
 
   
   sidebarLayout(
-      sidebarPanel(
+    sidebarPanel(
       
       dateRangeInput(inputId = "snapshotDate",label = "Date Range",
                      start = as.Date('2020-01-01'), end = as.Date('2020-12-31')),
       
       selectInput(inputId = "dieLocation",label = "Die Location",
-                  choices = sort(unique(stream$dieLocation)),selected = "M-085")
+                  choices = sort(unique(stream$dieLocation)),selected="M-085")
       
-                  ),
+    ),
     
-      mainPanel(
+    mainPanel(
       
-      streamgraphOutput(outputId = "streamPlot")
-               )
-                   )
-                )
+      streamgraphOutput(outputId = "streamPlot"),
+      h2(textOutput("CurrentInv"), style="text-align:right")
+    )
+  )
+)
 
 #Connecting UI Inputs to streamPlot Outputs
 server <- function(input,output){
-  
+
   output$streamPlot <- renderStreamgraph({
     
     filtered <-
@@ -51,14 +52,37 @@ server <- function(input,output){
       filter(dieLocation==input$dieLocation,
              snapshotDate>=input$snapshotDate[1],
              snapshotDate<=input$snapshotDate[2]
-             )
+      ) %>%
+      select(itemNumber,snapshotDate,quantityOnHand) %>%
+      arrange(desc(snapshotDate),desc(quantityOnHand))
     
-    streamgraph(filtered,key="itemNumber",value="quantityOnHand",date="snapshotDate",
+    latest=filtered[1,2]
+    
+    validate(need(nrow(filtered)!=0, "Adjust Date Range"))
+    
+    qual<-filtered[!duplicated(filtered$itemNumber),]
+    qual<-qual %>% arrange(desc(snapshotDate))
+    qual$seq<-1:length(qual$itemNumber)
+    
+    merge<-merge(filtered,qual[,c(1,4)],by="itemNumber")
+    merge$code<-paste0(merge$seq,".",merge$itemNumber)
+    merge$code<-formatC(merge$code,digits=ncar(merge$itemNumber),format='f')
+    
+    streamgraph(merge,key="code",value="quantityOnHand",date="snapshotDate",
                 offset="zero", height="800px", width="1000px",
-                left=70) 
-    
-                                          })
+                left=70,order="asis") %>%
+      sg_axis_x(tick_format = "%b-%y")
+  })
   
-                                  }
+    output$CurrentInv <- renderText({
+      current<-stream %>%
+        filter(dieLocation==input$dieLocation,
+               snapshotDate==latest
+               )
+    
+      paste0("Current Units On-Hand for ",input$dieLocation,": ",as.numeric(sum(current$quantityOnHand)))
+  })
+  
+}
 
 shinyApp(ui=ui,server=server)
